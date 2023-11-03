e2aef2b533734265d9a00b44684ffa3e091ade07
Datafox-JCP <datafox@outlook.com>
2023-11-03 08:29:14 -0600

    Initial Commit

---
+++ Nimble/NimbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbleApp.swift
+//  Nimble
+//
+//  Created by Juan Hernandez Pazos on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NimbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
